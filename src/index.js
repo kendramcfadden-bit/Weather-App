af335822dd609e448a491f88ee3e39601aeae2bc
kendramcfadden-bit <127974126+kendramcfadden-bit@users.noreply.github.com>
2023-04-07 15:58:45 -0600

    test

--- src/index.js
+++ src/index.js
@@ -47,39 +47,34 @@ let minute = now.getMinutes();
 // form.addEventListener("submit", search);
 
 //Forecast
-function displayForecast(){
-let forecastElement=document.querySelector("#forecast");
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
-let days = ["Thu","Fri","Sat","Sun"];
+  let days = ["Thu", "Fri", "Sat", "Sun"];
 
-
-let forecastHTML=`<div class="row">`;
-days.forEach(function(day){
-
-  forecastHTML= forecastHTML + `
-  <div class="weather-forecast" id="forecast">
-  
-    <div class="col-2">
-      <div class="forecast-day">Monday</div>
-      <!-- <img src="" alt=""> -->
-      <div class="forecast-icon">🌤️</div>
-      <div class="forecast-temperatures">
-        <span class="forecast-min-temp">32°F | </span><span class="forecast-max-temp">57°F</span>
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
       </div>
-  
-    </div>
-  </div>
-  
-  </div>
-  
   `;
+  });
 
-
-});
-
-
-forecastHTML= forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 //Search Engine 
