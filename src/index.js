13e13f5dd54c458052bcea1548ea9ee81bdc8901
kendramcfadden-bit <127974126+kendramcfadden-bit@users.noreply.github.com>
2023-04-08 15:35:48 -0600

    tried an older version of the api

--- src/index.js
+++ src/index.js
@@ -89,8 +89,8 @@ if (index<6) {
 function getForecast(coordinates){
   console.log(coordinates);
   let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl=`https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
-axios.get(apiUrl).then(displayForecast);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Search Engine 
